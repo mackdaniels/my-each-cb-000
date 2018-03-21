9f4ae489dae8e2f30312ecba327d74017b669b16
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-21 01:23:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,12 @@
 def my_each(array)
   if block_given?
     counter = 0
-    while counter <= array.length
+    while counter < array.length
       yield array[counter]
-      array[counter]
     end
+    array
+  else
+    "no block given"
+  end
   end
 end
