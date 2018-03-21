a5340106e5d348e8347da014dcacae7c1ef715e2
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-21 01:52:13 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array)
+  if block_given?
+    counter = 0
+    while counter < array.length
+      yield array[counter]
+      counter = counter + 1
+    end
+  end
+  array
 end
